158e79b410d62d4bfb5b71b781eaf330277d6c52
PutraMiaw <tangguhputra001@gmail.com>
2025-12-19 13:38:52 +0700

    Initial commit for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { HashRouter } from 'react-router-dom' // GANTI BrowserRouter
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter> {/* PAKAI HashRouter */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
